059b24808181b70729ed4dc19d369bec1d1b29ff
Kerat <tarek.marce@gmail.com>
2016-05-07 21:48:40 +0200

    Classe Grille finie, 2 mini méthode de la classe Case faites. Tarek (Samedi 7 Mai 22h)

--- src/Model/Case.java
+++ src/Model/Case.java
@@ -4,7 +4,22 @@ import java.awt.*;
 
 public class Case
 {
-	boolean devoilee, mine, drapeau;
-	int minesAdjacentes;
-	Point coord;
+	private boolean devoilee, mine, drapeau;
+	private int minesAdjacentes;
+	private Point coord;
+
+	/**
+	 * Marque la case comme une mine.
+	 * @author Tarek
+	 */
+	public void setAsMine(){
+		mine = true;
+	}
+	/**
+	 * Incremente le nombre de mine adjacentes
+	 * @author Tarek
+	 */
+	public void incrementeMinesAdjacentes(){
+		minesAdjacentes++;
+	}
 }
--- src/Model/Grille.java
+++ src/Model/Grille.java
@@ -2,8 +2,93 @@ package Model;
 
 public class Grille
 {
-	Case grille[][];
-	int longeur, largeur, nombreDeMines, casesVides, casesDevoiles;
+	private final static int LARGEUR_PAR_DEFAUT=8;
+	private final static int HAUTEUR_PAR_DEFAUT =8;
+	private final static int NBRE_DE_MINES_PAR_DEFAUT=4;
 
+	private Case grille[][];
+	private final int hauteur;
+	private final int largeur;
+	private final int nombreDeMines;
+	private final int casesVides;
+	private int casesDevoilees;
 
+	/**
+	 * Constructeur par défaut de Grille,
+	 * crée une nouvelle grille avec les dimensions par
+	 * défaut.
+	 * @author Tarek
+	 */
+	public Grille(){
+		this(HAUTEUR_PAR_DEFAUT,LARGEUR_PAR_DEFAUT,NBRE_DE_MINES_PAR_DEFAUT);
+	}
+
+	/**
+	 * Crée une nouvelle grille aux dimensions
+	 * transmises en parametre.
+	 * @param haut Hauteur de la grille.
+	 * @param larg Largeur de la grille.
+	 * @param nbreMines Nombre de mines à placer dans la grille.
+	 * @author Tarek
+     */
+	public Grille(int haut, int larg, int nbreMines){
+		hauteur = haut;
+		largeur=larg;
+		nombreDeMines=nbreMines;
+		this.newGrille();
+		this.placeMines();
+		casesVides=haut*larg-nbreMines;
+		casesDevoilees=0;
+	}
+
+	/**
+	 * Génère une nouvelle grille.
+	 * @author Tarek
+     */
+	private void newGrille(){
+		int i,j;
+		grille = new Case[hauteur][largeur];
+		for(i=0;i<hauteur;i++){
+			for(j=0;j<largeur;j++){
+				grille[i][j] = new Case();
+			}
+		}
+	}
+	/**
+	 * Place les mines dans la grille.
+	 * @author Tarek
+     */
+	private void placeMines(){
+		int i,j;
+		double probMine = (double)nombreDeMines/(hauteur*largeur);
+		int minesAPlace = nombreDeMines;
+		for(i=0;i<hauteur;i++){
+			for(j=0;j<largeur;j++){
+				if(minesAPlace != 0) {
+					if (Math.random() <= probMine) {
+						grille[i][j].setAsMine();
+						minesAPlace--;
+						if (i != 0 && j != 0) grille[i-1][j-1].incrementeMinesAdjacentes();
+						if (i != 0) grille[i-1][j].incrementeMinesAdjacentes();
+						if (i != 0 && j != largeur-1) grille[i-1][j+1].incrementeMinesAdjacentes();
+						if (j != largeur-1) grille[i][j+1].incrementeMinesAdjacentes();
+						if (i != hauteur-1 && j != largeur-1) grille[i+1][j+1].incrementeMinesAdjacentes();
+						if (i != hauteur-1) grille[i+1][j].incrementeMinesAdjacentes();
+						if (i != hauteur-1 && j != 0) grille[i+1][j-1].incrementeMinesAdjacentes();
+						if (j != 0) grille[i][j-1].incrementeMinesAdjacentes();
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Indique si la grille est gagnée (toutes les cases sont
+	 * dévoilées sauf les mines).
+	 * @return True si la grille est gagnée, False sinon.
+	 * @author Tarek
+     */
+	public boolean isGagnee(){
+		return casesDevoilees == casesVides;
+	}
 }
