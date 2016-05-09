c85f9197a0f081e1cde8d1318152a775302bc21d
Raphael-R-R <raphael.ragoomundun.act@openmailbox.org>
2016-05-09 19:54:39 +0200

    Control : Ajout variables d'instances et contructeurs de classe

--- src/Control/Control.java
+++ src/Control/Control.java
@@ -1,8 +1,38 @@
 package Control;
 
-public class Control
+import Model.Case;
+import Model.Chrono;
+
+public abstract class Control
 {
-	int i = 1;
+	protected Case case_;
+	protected Chrono chrono;
+	protected ControlMenu controlMenu;
+
+	/**
+	 * Constructeur par défaut de Control
+	 *
+	 * @author Raphael-R-R
+	 */
+
+	public Control() {}
+
+	/**
+	 * Constructeur de Control
+	 *
+	 * @param case_ Une référence vers une instance de la classe Case
+	 * @param chrono Une référence vers une instance de la classe Chrono
+	 * @param controlMenu Une référence vers une instance de la classe ControlMenu
+	 *
+	 * @author Raphael-R-R
+	 */
+
+	public Control(Case case_, Chrono chrono, ControlMenu controlMenu)
+	{
+		this.case_ = case_;
+		this.chrono = chrono;
+		this.controlMenu = controlMenu;
+	}
 }
 
 
