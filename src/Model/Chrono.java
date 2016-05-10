d4cc5b2caf350bf9b3bd41f9ea6b081226ea59f6
Raphael-R-R <raphael.ragoomundun.act@openmailbox.org>
2016-05-10 23:05:19 +0200

    Chrono : Ajout variables d'instaces, getters, setters, et quelques fonctions utiles.
    Difficile d'avancer plus que ça car la base de donnée n'est pas prête.

--- src/Model/Chrono.java
+++ src/Model/Chrono.java
@@ -1,5 +1,122 @@
 package Model;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Chrono {
-    float temps, meilleurTemps;
+    private float temps, meilleurTemps;
+	private Timer timer;
+	private Grille grille;
+
+	/**
+	 * Constructeur par défaut de chrono
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	public Chrono() {}
+
+	/**
+	 * Première surcharge du constructeur de Chrono
+	 *
+	 * Récupère que le meilleur temps, et prend en
+	 * référence une instance de Grille.
+	 * Les autres variables d'instances sont
+	 * initialisées par java.
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	public Chrono(Grille grille) {
+		this.grille = grille;
+
+		meilleurTemps = getMeilleurTempsFromDB();
+	}
+
+	/**
+	 * Démarre le timer qui permettra de calculer le temps écoulé
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	public void start() {
+		timer = new Timer(1000, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent actionEvent) {
+				setTemps(getTemps() + 1);
+			}
+		});
+	}
+
+	/**
+	 * Arrête le timer
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	public void stop() {
+		timer.stop();
+	}
+
+	/**
+	 * Retourne le temps actuel
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	public float getTemps() {
+		return temps;
+	}
+
+	/**
+	 * Retourne le meilleur temps
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	public float getMeilleurTemps() {
+		return meilleurTemps;
+	}
+
+	/**
+	 * Affecte le temps passé en paramètre au temps actuel
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	private void setTemps(float temps) {
+		this.temps = temps;
+	}
+
+	/**
+	 * Récupère le meilleur temps depuis la base de donnée
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	private float getMeilleurTempsFromDB() {
+
+	}
+
+	/**
+	 * Ajoute le meilleur temps à la base de donnée
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	private float setMeilleurTempsInDB() {
+
+	}
+
+	/**
+	 * Renvoie vrai si le temps actuel est le meilleur temps,
+	 * sinon faux
+	 *
+	 * @author : Raphael-R-R
+	 */
+
+	private boolean isMeilleurTemps() {
+
+	}
 }
