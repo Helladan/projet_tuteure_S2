c9f4ad74f225536d3080520403a98719f727773d
DonCorentin <coco.della@hotmail.fr>
2016-05-12 08:49:16 +0200

    Création du controlMenu Je crois que c'est fini

--- src/Control/ControlMenu.java
+++ src/Control/ControlMenu.java
@@ -1,5 +1,39 @@
 package Control;
+import View.*;
+import java.awt.event.*;
 
-public class ControlMenu extends Control
-{
+/**
+ * @author Corentin
+ */
+
+public class ControlMenu extends Control implements ActionListener {
+    Fenetre fenetre;
+    fenetre.setControlMenu(this);
+
+
+    public ControlMenu(){
+
+    }
+
+    public ControlMenu(Fenetre fenetre){
+        this.fenetre = fenetre;
+    }
+
+    /**
+     * Gère le menu en accèdant aux fontions crées dans la fenêtre
+     * @param e
+     */
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == fenetre.mItemNouvellePartie){
+            fenetre.restart();
+        }
+        else if(e.getSource() == fenetre.mItemScores){
+            fenetre.afficherScores();
+        }
+        else if(e.getSource() == fenetre.mItemQuitter){
+            fenetre.quitter();
+        }
+
+    }
 }
