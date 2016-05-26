8617f3f286afba8f1eedb4e8e9dac0672c7790e8
Kerat <tarek.marce@gmail.com>
2016-05-27 00:58:11 +0200

    Affichage : Chrono mvc (ne marche pas pour l'instant) (Vendredi 27 Mai 01h00)

--- src/Model/Chrono.java
+++ src/Model/Chrono.java
@@ -48,6 +48,7 @@ public class Chrono {
 
 	public void start() {
 		timer = new Timer(1000, new ActionListener() {
+			@Override
 			public void actionPerformed(ActionEvent actionEvent) {
 				setTemps(getTemps() + 1);
 			}
